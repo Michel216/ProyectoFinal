f3e0531eb8cc27709cbda3bb80db5099d34d3ab6
GodKronosJke <dragondragonred77@gmail.com>
2024-09-30 21:56:45 -0500

    followup

--- backend/src/helpers/followup.js
+++ backend/src/helpers/followup.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:src/helpers/followup.js
 const e = require("express")
 const Assignment = require("../models/assignment.js")
 const Followup = require("../models/followup.js")
@@ -18,38 +17,19 @@ const followUpHelper = {
         } return existAssignment
     },
     
-    // validateInstrustor: async(idinstructor) => {
-    //     let existInstructor = await Instructor.findById(idinstructor)
-    //     if(!existInstructor){
-    //         throw new Error ("El id de instructor no existe")
-    //     }
-    // },
-}
-=======
-const Assignment = require("../models/assignment.js")
-const Followup = require("../models/followup.js")
-// const Instructor = require("../models/instructor.js")
-const followUpHelper = {
-    validateId:async(id) => {
-        let existsId = await Followup.findById(id)
-        if(!existsId){
-            throw new Error("El seguimiento no existe en la base de datos")
-        }
-    },
+    validateStatus:  (status) =>{
+        const Status =[1,2,3,4];
+        if(!Status.includes(status)){
+            throw new Error("El estado debe ser 1, 2,3,4");
+    }
+    return true;
+},
 
-    validateAssignment: async(idassignment) =>{
-        let existAssignment = await Assignment.findById(idassignment)
-        if(!existAssignment){
-            throw new Error("El id de asignación no existe ")
+    validateInstrustor: async(idinstructor) => {
+        let existInstructor = await axios.get(`http://89.116.49.65:4500/api/instructors/${idinstructor}`)
+        if(!existInstructor){
+            throw new Error ("El id de instructor no existe")
         }
     },
-    
-    // validateInstrustor: async(idinstructor) => {
-    //     let existInstructor = await Instructor.findById(idinstructor)
-    //     if(!existInstructor){
-    //         throw new Error ("El id de instructor no existe")
-    //     }
-    // },
 }
->>>>>>> 3b608c9334b4418ac5bbb3e8699c9d00f4ea4889:backend/src/helpers/followup.js
 module.exports = followUpHelper;
