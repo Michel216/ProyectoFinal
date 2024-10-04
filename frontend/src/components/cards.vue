cce8a4d61501dedbc0403b4f9ebe46991f8f7cbb
GodKronosJke <dragondragonred77@gmail.com>
2024-10-03 22:40:30 -0500

    cards

---
+++ frontend/src/components/cards.vue
@@ -0,0 +1,98 @@
+<template>
+    <div class="q-pa-md row items-start q-gutter-md">
+  
+      <q-card class="my-card">
+        <q-card-section class="bg-green">
+          <div class="text-h6 text-center">Aprendiz</div>
+        </q-card-section>
+          <img src="https://sena.edu.co/Style%20Library/alayout/images/logoSena.png">
+  
+        <q-card-actions vertical>
+          <q-btn flat>Ingresar</q-btn>
+        </q-card-actions>
+      </q-card>
+      
+      <q-card class="my-card">
+        <q-card-section class="bg-green" >
+          <div class="text-h6 text-center ">Registro</div>
+        </q-card-section>
+          <img src="https://sena.edu.co/Style%20Library/alayout/images/logoSena.png">
+  
+        <q-card-actions vertical>
+          <q-btn flat>Ingresar</q-btn>
+        </q-card-actions>
+      </q-card>
+  
+      <q-card class="my-card">
+        <q-card-section class="bg-green">
+          <div class="text-h6 text-center">Modalidad</div>
+        </q-card-section>
+          <img src="https://sena.edu.co/Style%20Library/alayout/images/logoSena.png">
+  
+        <q-card-actions vertical>
+          <q-btn flat>Ingresar</q-btn>
+        </q-card-actions>
+      </q-card>
+  
+      <q-card class="my-card">
+        <q-card-section class="bg-green">
+          <div class="text-h6 text-center">Asignaciones</div>
+        </q-card-section>
+          <img src="https://sena.edu.co/Style%20Library/alayout/images/logoSena.png">
+  
+        <q-card-actions vertical>
+          <q-btn flat>Ingresar</q-btn>
+        </q-card-actions>
+      </q-card>
+  
+      <q-card class="my-card">
+        <q-card-section class="bg-green">
+          <div class="text-h6 text-center">Seguimientos</div>
+        </q-card-section>
+          <img src="https://sena.edu.co/Style%20Library/alayout/images/logoSena.png">
+  
+        <q-card-actions vertical>
+          <q-btn flat>Ingresar</q-btn>
+        </q-card-actions>
+      </q-card>
+  
+      <q-card class="my-card">
+        <q-card-section class="bg-green">
+          <div class="text-h6 text-center">Bitacoras</div>
+        </q-card-section>
+          <img src="https://sena.edu.co/Style%20Library/alayout/images/logoSena.png">
+  
+        <q-card-actions vertical>
+          <q-btn flat>Ingresar</q-btn>
+        </q-card-actions>
+      </q-card>
+  
+      <q-card class="my-card">
+        <q-card-section class="bg-green">
+          <div class="text-h6 text-center">Log</div>
+        </q-card-section>
+          <img src="https://sena.edu.co/Style%20Library/alayout/images/logoSena.png">
+  
+        <q-card-actions vertical>
+          <q-btn flat>Ingresar</q-btn>
+        </q-card-actions>
+      </q-card>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    setup () {
+      return {
+        lorem: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      }
+    }
+  }
+  </script>
+  
+  <style lang="sass" scoped>
+  .my-card
+    width: 100%
+    max-width: 250px
+  </style>
+  
