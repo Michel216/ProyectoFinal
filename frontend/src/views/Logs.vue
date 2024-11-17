ff5aa0decb7309ee23827c6532049c514dba1763
jinnethh <jinnethcruz724@gmail.com>
2024-11-17 11:55:58 -0500

    ajustes login

--- frontend/src/views/Logs.vue
+++ frontend/src/views/Logs.vue
@@ -4,37 +4,71 @@
       <div class="login-header">
         <h5>ETAPAS PRODUCTIVAS</h5>
       </div>
-      <img src="https://repforacat.com/images/LOGO-SENA.png" alt="Logo SENA" class="logo">
+      <img
+        src="https://repforacat.com/images/LOGO-SENA.png"
+        alt="Logo SENA"
+        class="logo"
+      />
       <h2 class="login-title">LOGIN</h2>
       <div class="container-form">
         <form @submit.prevent="handleSubmit">
           <div class="form-group">
-            <label for="rol">Rol</label>
-            <select id="rol" v-model="rol" class="input-field">
-              <option value="APRENDIZ">CONSULTOR</option>
-              <option value="ADMIN">ADMIN</option>
-              <option value="INSTRUCTOR">INSTRUCTOR</option>
-            </select>
+  <label for="rol">Rol</label>
+  <select id="rol" v-model="rol" class="input-field">
+    <option value="" disabled selected hidden>SELECCIONA UN ROL</option>
+    <option value="APRENDIZ">CONSULTOR</option>
+    <option value="ADMIN">ADMIN</option>
+    <option value="INSTRUCTOR">INSTRUCTOR</option>
+  </select>
+</div>
+
+
+          <div
+            class="form-group"
+            v-if="rol === 'APRENDIZ' || rol === 'INSTRUCTOR' || rol === 'ADMIN'"
+          >
+            <q-input
+              outlined
+              class="q-mt-sm"
+              v-model="email"
+              label="Correo Electrónico"
+              :rules="[
+                (val) =>
+                  (val && val.trim() !== '') ||
+                  'Por favor ingresa tu correo electrónico',
+              ]"
+            />
           </div>
 
-          <div class="form-group" v-if="rol === 'APRENDIZ' || rol === 'INSTRUCTOR' || rol === 'ADMIN'">
-          
-            <input type="email" id="email" v-model="email" required class="input-field" placeholder="EMAIL" />
-          </div>
           <div class="form-group" v-if="rol === 'APRENDIZ'">
-           
-            <input type="text" id="cedula" v-model="cedula" required class="input-field" placeholder="NUM.DOCUMENTO"/>
-          </div>           
+            <q-input
+              outlined
+              class="q-mt-sm"
+              v-model="document"
+              label="Num. Documento"
+              :rules="[
+                (val) =>
+                  (val && val.trim() !== '') ||
+                  'Por favor ingresa tu Num de documento',
+              ]"
+            />
+          </div>
+
           <div
             class="form-group"
             v-if="rol === 'ADMIN' || rol === 'INSTRUCTOR'"
           >
-  
             <q-input
               outlined
               class="q-mt-md"
               v-model="password"
-              :type="isPwd ? 'password' : 'text'" placeholder="CONTRASEÑA"
+              :type="isPwd ? 'password' : 'text'"
+              placeholder="CONTRASEÑA"
+              :rules="[
+                (val) =>
+                  (val !== null && val.trim() !== '') ||
+                  'Por favor ingresa tu contraseña',
+              ]"
             >
               <template v-slot:append>
                 <q-icon
@@ -46,173 +80,209 @@
             </q-input>
           </div>
 
-          <Btn :label='btnLabel' :onClickFunction='ClickFunctionLogin' />
+      
+          <q-btn
+            :label="btnLabel"
+            :loading="isLoading"
+            @click="ClickFunctionLogin"
+            color="primary"
+            class="full-width"
+            :disable="isLoading"
+            loading-label="Iniciando sesión..."
+          />
         </form>
       </div>
       <p>
-        <a @click="forgotPassword" class="forgot-password">Olvidé mi contraseña</a>
+        <a @click="forgotPassword" class="forgot-password"
+          >Olvidé mi contraseña</a
+        >
       </p>
     </div>
   </div>
 </template>
 
+
+
 <script setup>
-import { ref } from 'vue';
-import { useRouter } from 'vue-router';
-import Btn from '../components/buttons/Button.vue';
-import { postDataLogin } from '../services/apiRepfora.js';
-import { postLogin } from '../services/apiClient.js';
-import { useAuthStore } from '../store/useAuth.js';
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import Btn from "../components/buttons/Button.vue";
+import { postDataLogin } from "../services/apiRepfora.js";
+import { postLogin } from "../services/apiClient.js";
+import { useAuthStore } from "../store/useAuth.js";
 
 const router = useRouter();
 const authStore = useAuthStore();
 
-let btnLabel = "INICIAR SESIÓN"
-const rol = ref('');
-const email = ref('');
-const cedula = ref('');
-const password = ref('');
+let btnLabel = "INICIAR SESIÓN";
+const rol = ref("");
+const email = ref("");
+const cedula = ref("");
+const password = ref("");
 const isPwd = ref(true);
+const isLoading = ref(false); 
 
 const handleSubmit = async () => {
-    console.log('Inicio de sesión con rol:', rol.value, email.value, 'y contraseña:', password.value);
-    try {
-        let endpoint;
-        let data;
+ 
+  if (!rol.value) {
+    alert("Por favor selecciona un rol.");
+    return;
+  }
+  console.log(
+    "Inicio de sesión con rol:",
+    rol.value,
+    email.value,
+    "y contraseña:",
+    password.value
+  );
+  isLoading.value = true; 
 
-        if (rol.value === 'ADMIN') {
-            data = await postDataLogin('/users/login', {role:rol.value, email: email.value, password: password.value });
-        } else if (rol.value === 'INSTRUCTOR') {
-          data = await postDataLogin('/instructors/login', {role:rol.value, email: email.value, password: password.value });
-        } 
+  try {
+    let endpoint;
+    let data;
 
-        if (rol.value === 'APRENDIZ') {
-            data = await postLogin('/apprentice/loginapprentice', { institutionalEmail: email.value, numDocument: cedula.value });
-            console.log('Datos de inicio de sesión del aprendiz:', data);
-        }
-
-        if (!data) {
-            console.error('No se recibió respuesta válida del servidor');
-            return;
-        }
-
-        // Almacena el token y el rol en el store
-        authStore.setToken(data.token, rol.value, data.name, data.email);
-
-        // Redirigir a la página de inicio
-        router.replace("/Home");
-    } catch (error) {
-        console.error('Error durante el inicio de sesión:', error);
+    if (rol.value === "ADMIN") {
+      data = await postDataLogin("/users/login", {
+        role: rol.value,
+        email: email.value,
+        password: password.value,
+      });
+    } else if (rol.value === "INSTRUCTOR") {
+      data = await postDataLogin("/instructors/login", {
+        role: rol.value,
+        email: email.value,
+        password: password.value,
+      });
     }
-};
 
+    if (rol.value === "APRENDIZ") {
+      data = await postLogin("/apprentice/loginapprentice", {
+        institutionalEmail: email.value,
+        numDocument: cedula.value,
+      });
+      console.log("Datos de inicio de sesión del aprendiz:", data);
+    }
+
+    if (!data) {
+      console.error("No se recibió respuesta válida del servidor");
+      return;
+    }
+
+    // Almacena el token y el rol en el store
+    authStore.setToken(data.token, rol.value, data.name, data.email);
+
+    // Redirigir a la página de inicio
+    router.replace("/Home");
+  } catch (error) {
+    console.error("Error durante el inicio de sesión:", error);
+  }
+};
 
 const forgotPassword = () => {
   router.replace("./forgotpassword");
-  console.log('Olvidé mi contraseña');
-}
+  console.log("Olvidé mi contraseña");
+};
 
 const ClickFunctionLogin = async () => {
-handleSubmit()
-}
+  handleSubmit();
+};
 </script>
 
 <style scoped>
 .login-container {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-    
-    background-color: #f0f0f0;
-  }
-  
-  .login-container p{
-    margin-top: 10px !important;
-    margin-bottom: 10px !important;
-    text-decoration: underline;
-  }
-  
-  .container-form{
-    padding: 20px !important;
-  }
-  
- .login-box {
-    width: 40%;
-    /* padding: 20px; */
-    border-radius: 10px;
-    background-color: #fff;
-    box-shadow: 0 5px 5px -3px #0003, 0 8px 10px 1px #00000024,
-    0 3px 14px 2px #0000001f;    text-align: center;
-    /* margin: 40% 0; */
-    height: auto;
-  }
-  
-  .login-header {
-    background-color: #2F7D32;
-    padding: 30px;
-    border-radius: 8px 8px 0 0;
-  }
-  
-  .login-header h5 {
-    color: white;
-    /* font-size: 40px; */
-    font-weight: bold;
-    margin: 0;
-  }
-  
-  .logo {
-    width: 100px;
-    /* margin: 20px 0; */
-  }
-  
-  .login-title {
-    font-size: 25px;
-    /* margin-bottom: 10px; */
-    font-weight: bold;
-    margin: 0;
-  }
-  
-  
-  .form-group {
-    margin-bottom: 20px;
-  }
-  .form-group label{
-    font-weight: bold !important;
-  }
-  
-  .input-field {
-    width: 100%;
-    padding: 10px;
-    border: 1px solid #ccc;
-    border-radius: 5px;
-    font-size: 14px;
-  }
-  
-  
-  .login-button {
-    width: 30%;
-    padding: 10px;
-    background-color: #2F7D32;
-    color: white;
-    border: none;
-    border-radius: 5px;
-    font-size: 16px;
-    cursor: pointer;
-  }
-  
-  .login-button:hover {
-    background-color: #246024;
-    font-weight: 600;
-  }
-  
-  .forgot-password {
-    color: #2F7D32;
-    text-decoration: none;
-  }
-  
-  .forgot-password:hover {
-    text-decoration: underline;
-    font-weight: bold !important;
-  }
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+
+  background-color: #f0f0f0;
+}
+
+.login-container p {
+  margin-top: 10px !important;
+  margin-bottom: 10px !important;
+  text-decoration: underline;
+}
+
+.container-form {
+  padding: 20px !important;
+}
+
+.login-box {
+  width: 40%;
+  /* padding: 20px; */
+  border-radius: 10px;
+  background-color: #fff;
+  box-shadow: 0 5px 5px -3px #0003, 0 8px 10px 1px #00000024,
+    0 3px 14px 2px #0000001f;
+  text-align: center;
+  /* margin: 40% 0; */
+  height: auto;
+}
+
+.login-header {
+  background-color: green;
+  padding: 30px;
+  border-radius: 8px 8px 0 0;
+}
+
+.login-header h5 {
+  color: white;
+  /* font-size: 40px; */
+  font-weight: bold;
+  margin: 0;
+}
+
+.logo {
+  width: 100px;
+  /* margin: 20px 0; */
+}
+
+.login-title {
+  font-size: 25px;
+  /* margin-bottom: 10px; */
+  font-weight: bold;
+  margin: 0;
+}
+
+.form-group {
+  margin-bottom: 20px;
+}
+.form-group label {
+  font-weight: bold !important;
+}
+
+.input-field {
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  font-size: 14px;
+}
+
+.login-button {
+  width: 30%;
+  padding: 10px;
+  background-color: green;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  font-size: 16px;
+  cursor: pointer;
+}
+
+.login-button:hover {
+  background-color: green;
+  font-weight: 600;
+}
+
+.forgot-password {
+  color: green;
+  text-decoration: none;
+}
+
+.forgot-password:hover {
+  text-decoration: underline;
+  font-weight: bold !important;
+}
 </style>
