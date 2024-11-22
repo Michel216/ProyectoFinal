f22ad73351892928846fdb867a45e7ca41d6b85b
GodKronosJke <dragondragonred77@gmail.com>
2024-11-21 22:29:28 -0500

    boton estado

--- frontend/src/components/tables/SecondTable.vue
+++ frontend/src/components/tables/SecondTable.vue
@@ -4,8 +4,17 @@
       <template v-slot:body-cell-options="scope">
         <q-td :props="scope">
           <div class="q-pa-md" align="center" v-if="role === 'ADMIN'">
-            <q-select outlined v-model="scope.row.status" style="width: 200px;" :options="options" label="Estado"
-              emit-value map-options @update:model-value="updateStatus($event, scope.row._id)" default="Ejecutada" />
+            <q-select
+              outlined
+              v-model="scope.row.status"
+              style="width: 200px"
+              :options="options"
+              label="Estado"
+              emit-value
+              map-options
+              @update:model-value="updateStatus($event, scope.row._id)"
+              default="Ejecutada"
+            /> 
           </div>
           <div v-if="role === 'INSTRUCTOR'">
             <strong v-if="scope.row.status === 1">Programado</strong>
@@ -17,26 +26,52 @@
           </div>
         </q-td>
       </template>
+      <template v-slot:body-cell-status="scope">
+        <q-td :props="scope">
+          <q-btn
+          :label="scope.row.status ? 'Activo' : 'Inactivo'"
+          :color="scope.row.status ? 'green' : 'red'"
+          :unelevated="true"
+          @click="toggleState(scope.row)"
+          />
+        </q-td>
+      </template>
       <!--valida que el tipo de la bitácora sea de 1 a 4. Programado: 1, Ejecutado: 2, Pendiente: 3, Verificado: 4 -->
       <template v-slot:body-cell-validateHours="scope">
         <q-td :props="scope">
           <div class="q-pa-md">
             <q-checkbox />
           </div>
-
         </q-td>
       </template>
       <template v-slot:body-cell-observations="scope">
         <q-td :props="scope">
           <div class="q-pa-md q-gutter-xs">
-            <q-btn round color="primary" icon="search" @click="handleClick(scope.row._id, true)" />
-            <q-btn round color="primary" icon="add_circle_outline" @click="handleClick(scope.row._id, false)" />
+            <q-btn
+              round
+              color="primary"
+              icon="search"
+              @click="handleClick(scope.row._id, true)"
+            />
+            <q-btn
+              round
+              color="primary"
+              icon="add_circle_outline"
+              @click="handleClick(scope.row._id, false)"
+            />
           </div>
         </q-td>
       </template>
       <template v-slot:body-cell-apprentice="props">
         <q-td :props="props" align="center">
-          <Btn dense unelevated round color="primary" @click="toggleSeeApprentice(props.row)" label=" ">
+          <Btn
+            dense
+            unelevated
+            round
+            color="primary"
+            @click="toggleSeeApprentice(props.row)"
+            label=" "
+          >
             <template v-slot:prepend>
               <font-awesome-icon icon="eye" />
             </template>
@@ -51,25 +86,29 @@
       </template>
 
       <template v-slot:body-cell-statusApprentice="scope">
-  <q-td :props="scope">
-    <q-btn
-      :label="scope.row.status === 4 ? 'Certificado' : 'Por certificar'"
-      :color="scope.row.status === 4 ? 'primary' : 'grey'"
-      :unelevated="true"
-         :text-color="scope.row.status === 4 ? 'white' : 'black'"
-      class="status-btn"
-    />
-  </q-td>
-</template>
+        <q-td :props="scope">
+          <q-btn
+            :label="scope.row.status === 4 ? 'Certificado' : 'Por certificar'"
+            :color="scope.row.status === 4 ? 'primary' : 'grey'"
+            :unelevated="true"
+            :text-color="scope.row.status === 4 ? 'white' : 'black'"
+            class="status-btn"
+          />
+        </q-td>
+      </template>
 
       <template v-slot:body-cell-certificationDoc="scope">
         <q-td :props="scope">
-          <a :href="scope.row.certificationDoc" target="_blank"><font-awesome-icon icon="fa-solid fa-folder" /></a> 
+          <a :href="scope.row.certificationDoc" target="_blank"
+            ><font-awesome-icon icon="fa-solid fa-folder"
+          /></a>
         </q-td>
       </template>
       <template v-slot:body-cell-judymentPhoto="scope">
         <q-td :props="scope">
-          <a :href="scope.row.judymentPhoto" target="_blank"><font-awesome-icon icon="fa-solid fa-folder" /></a> 
+          <a :href="scope.row.judymentPhoto" target="_blank"
+            ><font-awesome-icon icon="fa-solid fa-folder"
+          /></a>
         </q-td>
       </template>
     </q-table>
@@ -77,49 +116,58 @@
 </template>
 
 <script setup>
-import { ref, computed } from 'vue';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { useRouter } from 'vue-router';
-import { faMagnifyingGlass, faEye, faFolder } from '@fortawesome/free-solid-svg-icons';
+import { ref, computed } from "vue";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { useRouter } from "vue-router";
+import {
+  faMagnifyingGlass,
+  faEye,
+  faFolder,
+} from "@fortawesome/free-solid-svg-icons";
 import Btn from "./../buttons/Button.vue";
-import { getData } from './../../services/apiClient' 
-import { useAuthStore } from '../../store/useAuth';
+import { getData } from "./../../services/apiClient";
+import { useAuthStore } from "../../store/useAuth";
 
 const authStore = useAuthStore();
-const role = computed(() => authStore.getRole()); 
+const role = computed(() => authStore.getRole());
 library.add(faMagnifyingGlass, faEye, faFolder);
 const router = useRouter();
-let loading = ref(false)
+let loading = ref(false);
 const props = defineProps({
   rows: {
     type: Array,
-    required: true
+    required: true,
   },
   columns: {
     type: Array,
-    required: true
+    required: true,
   },
   options: {
     type: Array,
-    required: true
+    required: true,
   },
   onUpdateStatus: {
     type: Function,
-    required: true
+    required: true,
   },
   val: {
-    type: String
+    type: String,
   },
   onClickFunction: {
-    type: Function
+    type: Function,
   },
   toggleSeeApprentice: {
     type: Function,
     required: true,
-  }
-
+  },
 });
+function toggleState(row) {
+  row.status = !row.status;
+  console.log(`El estado del programa con ID ${row._id} es ahora ${row.status ? 'Activo' : 'Inactivo'}`);
+  updateStatus(row.status, row._id);
+}
+
 
 // Combinar las columnas pasadas con las columnas de acciones "editar" y "activar"
 // const combinedColumns = computed(() => [
@@ -140,7 +188,7 @@ function updateStatus(status, row) {
 }
 
 function handleClick(row, change) {
-  props.onClickFunction(row, change) // Emite el evento al componente padre
+  props.onClickFunction(row, change); // Emite el evento al componente padre
 }
 // const emit = defineEmits(['openModal']);
 
@@ -155,8 +203,8 @@ function handleClick(row, change) {
 
 <style scoped>
 .status-btn {
-
-  border-radius: 8px; /* Bordes redondeados */
+  border-radius: 8px;
+  /* Bordes redondeados */
   font-size: 12px;
 }
 </style>
