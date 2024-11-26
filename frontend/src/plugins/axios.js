5487c529da960d5d6c0b855472d9bb2c4fdce0e8
santixdan <danysilva.2202@gmail.com>
2024-11-25 22:22:39 -0500

    hacer token

--- frontend/src/plugins/axios.js
+++ frontend/src/plugins/axios.js
@@ -1,7 +1,4 @@
 import axios from 'axios'
-import router from "./../router/routes.js"
-import { isTokenExpired } from "./../store/useAuth.js"
-
 const token = localStorage.getItem('token');  // Ejemplo de cómo obtener el token
 // "token": token,
 const apiClient = axios.create({
@@ -12,31 +9,6 @@ const apiClient = axios.create({
         // 'Content-Type': 'application/json',
         "token": token
     }
-});
-
-// Interceptar las respuestas
-apiClient.interceptors.response.use(
-    (response) => response, // Respuesta exitosa
-    (error) => {
-        if (error.response && error.response.status === 401) {
-            // Token inválido o expirado
-            localStorage.removeItem('token'); // Eliminar token
-            router.push('/'); // Redirigir al login
-        }
-        return Promise.reject(error);
-    }
-);
-
-// Interceptar las solicitudes para verificar el token antes de enviarlas
-apiClient.interceptors.request.use((config) => {
-    const token = localStorage.getItem('token');
-    if (isTokenExpired(token)) {
-        localStorage.removeItem('token');
-        router.push('/'); // Redirigir al login si el token está vencido
-        throw new axios.Cancel('Token expired'); // Cancelar la solicitud
-    }
-    config.headers["token"] = token; // Actualizar el token en los headers
-    return config;
 });
 
 export default apiClient;
--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -15,7 +15,6 @@ import Register from "../views/Register.vue";
 import { useAuthStore } from '../store/useAuth.js'; 
 import Instructors from "../views/Instructors.vue";
 import Layout from "./../layouts/layout.vue";
-import { isTokenExpired } from './../store/useAuth.js'
 
 // Definir la función de autenticación
 const auth = (to, from, next) => {
@@ -88,16 +87,19 @@ router.beforeEach((to, from, next) => {
     const token = authStore.getToken();
     const role = authStore.getRole();
 
-    if (!token || isTokenExpired(token)) {
-        // Eliminar token si está vencido o no existe
-        localStorage.removeItem('token');
-        next({ path: '/' }); // Redirigir al login
+    // Verificar si no está autenticado y la ruta requiere autenticación
+    if (to.meta.requiresAuth && !token) {
+        next({ path: '/' }); // Redirigir al login si no hay token
     } 
-    // Verificar si la ruta requiere autenticación y el rol no coincide
+    // Verificar si es "APRENDIZ" y está intentando acceder a algo que no sea "Binnacles"
+    // else if (role === 'CONSULTOR' && to.path !== '/HomeApprentice') {
+    //     next({ path: '/HomeApprentice' }); // Redirigir automáticamente a la única ruta permitida
+    // } 
+    // Verificar si el rol no coincide con el rol requerido por la ruta
     else if (to.meta.rol && !to.meta.rol.includes(role)) {
-        next({ path: '/' }); // Redirigir si no tiene permisos
+        next({ path: '/login' }); // Redirigir si el rol no coincide
     } else {
-        next(); // Continuar normalmente
+        next(); // Continuar a la ruta solicitada
     }
 });
 
