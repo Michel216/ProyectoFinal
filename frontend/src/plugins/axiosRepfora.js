f7a35b336b5b2970bca983d9715116fb8c3cacc9
santixdan <danysilva.2202@gmail.com>
2024-12-02 22:27:38 -0500

    cambios

--- frontend/src/plugins/axiosRepfora.js
+++ frontend/src/plugins/axiosRepfora.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const token = localStorage.getItem('token');  
 
 const apiRepfora = axios.create({
-    baseURL: import.meta.env.VITE_URL_REPFORA,
+    baseURL: 'http://89.116.49.65:4500/api',
     headers: {
         "token": token
     }
